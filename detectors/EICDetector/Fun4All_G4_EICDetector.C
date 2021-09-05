2dac93344faafe8e1e18c59d4d0fb05e30076973
Bill <wenliang.billlee@gmail.com>
2021-09-05 18:14:02 -0400

    Turned off the forward pipe

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -68,6 +68,7 @@ int Fun4All_G4_EICDetector(
   // used for both beamline setting and for the event generator crossing boost
   // Enable::IP6 = true;
   Enable::IP8 = true;
+  G4PIPE::use_forward_pipes = false;
 
   // Setting proton beam pipe energy. If you don't know what to set here, leave it at 275 
   // Enable::HFARFWD_ION_ENERGY=275;
