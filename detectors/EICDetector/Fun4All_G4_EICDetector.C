c3da76e0b34059dd201a0911c582548ede397a56
Bill <wenliang.billlee@gmail.com>
2021-09-05 18:24:57 -0400

    New branch

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -77,7 +77,7 @@ int Fun4All_G4_EICDetector(
   // 110 GeV Pb beam 
   // IF you are not Mark or Jackson, then you should not use this energy setting
   // Enable::HFARFWD_ION_ENERGY=275*110/108.4;
-  Enable::HFARFWD_ION_ENERGY=41;
+  Enable::HFARFWD_ION_ENERGY=275;
 
   // Either:
   // read previously generated g4-hits files, in this case it opens a DST and skips
