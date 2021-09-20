db650a55c4c8b94fa16ef873a4774e67da9afaa6
Bill <wenliang.billlee@gmail.com>
2021-09-20 02:32:06 -0400

    82 GeV beamline is created for 41 GeV/nucleon study for e+He4

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -70,7 +70,7 @@ int Fun4All_G4_EICDetector(
   // Enable::IP8 = true;
 
   // Setting proton beam pipe energy. If you don't know what to set here, leave it at 275 
-  Enable::HFARFWD_ION_ENERGY=220;
+  Enable::HFARFWD_ION_ENERGY=82;
 
   // Either:
   // read previously generated g4-hits files, in this case it opens a DST and skips
