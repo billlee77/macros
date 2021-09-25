221a1233f69c1d36f3ac99d3606440c20532a736
Bill <wenliang.billlee@gmail.com>
2021-09-25 11:49:03 -0400

    Energy coice added

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -70,7 +70,8 @@ int Fun4All_G4_EICDetector(
   // Enable::IP8 = true;
 
   // Setting proton beam pipe energy. If you don't know what to set here, leave it at 275
-  Enable::HFARFWD_ION_ENERGY = 82;
+  // Energy choice: 41, 62, 82, 100, 165, 220, 275
+  Enable::HFARFWD_ION_ENERGY = 275;
 
   // Either:
   // read previously generated g4-hits files, in this case it opens a DST and skips
