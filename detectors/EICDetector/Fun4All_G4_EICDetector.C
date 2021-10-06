2f40ff8f2e250a9fd51586add877c84f6f34f49e
Bill <wenliang.billlee@gmail.com>
2021-10-06 18:27:17 -0400

    41 GeV

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -71,7 +71,7 @@ int Fun4All_G4_EICDetector(
   G4PIPE::use_forward_pipes = false;
   G4WORLD::WorldMaterial = "G4_Galactic"; // set to G4_GALACTIC for material scans
 
-  // Setting proton beam pipe energy. If you don't know what to set here, leave it at 275 
+  Enable::HFARFWD_ION_ENERGY = 41;
   // Enable::HFARFWD_ION_ENERGY=275;
 
   // The following this the special steering requested by Mark to steer 
