64c49fbc73fcfa7bfdd408e3474c6a57016ded54
Bill <wenliang.billlee@gmail.com>
2021-12-07 14:15:38 -0500

    IP8 275 GeV

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -66,8 +66,10 @@ int Fun4All_G4_EICDetector(
 
   // switching IPs by comment/uncommenting the following lines
   // used for both beamline setting and for the event generator crossing boost
-  Enable::IP6 = true;
-  // Enable::IP8 = true;
+  // Enable::IP6 = true;
+  Enable::IP8 = true;
+  G4PIPE::use_forward_pipes = false;
+  G4WORLD::WorldMaterial = "G4_Galactic"; // set to G4_GALACTIC for material scans
 
   // Setting proton beam pipe energy. If you don't know what to set here, leave it at 275
   Enable::HFARFWD_ION_ENERGY = 275;
@@ -264,7 +266,7 @@ int Fun4All_G4_EICDetector(
   // whether to simulate the Be section of the beam pipe
   Enable::PIPE = true;
   // If need to disable EIC beam pipe extension beyond the Be-section:
-  G4PIPE::use_forward_pipes = true;
+  // G4PIPE::use_forward_pipes = true;
   //EIC hadron far forward magnets and detectors. IP6 and IP8 are incompatible (pick either or);
   Enable::HFARFWD_MAGNETS = true;
   Enable::HFARFWD_VIRTUAL_DETECTORS = true;
