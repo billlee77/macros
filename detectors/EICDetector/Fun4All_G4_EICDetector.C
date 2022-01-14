b30790bac7ea070f0ef3c6bf61e84ea7279fe53e
Bill <wenliang.billlee@gmail.com>
2022-01-14 02:41:17 -0500

    5x100

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -81,10 +81,10 @@ int Fun4All_G4_EICDetector(
   //Enable::IP8 = true;
 
   // Setting proton beam pipe energy. If you don't know what to set here, leave it at 275
-  Enable::HFARFWD_ION_ENERGY = 275;
+  Enable::HFARFWD_ION_ENERGY = 100;
 
   // Setting electron beam pipe energy. If you don't know what to set here, leave it at 18
-  Enable::HFARBWD_E_ENERGY = 18;
+  Enable::HFARBWD_E_ENERGY = 5;
 
   // G4PIPE::use_forward_pipes = false;
 
