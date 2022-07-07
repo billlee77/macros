4e4a73adc2db1bce47fbed087d290694b7dad267
billlee77 <wenliang.billlee@gmail.com>
2022-07-07 07:06:36 -0400

    18x100 hi-div IP6

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -82,7 +82,7 @@ int Fun4All_G4_EICDetector(
   // Option: 110x18, 110x10, 110x5, 41x5
 
   // Setting proton beam pipe energy. If you don't know what to set here, leave it at 275
-  Enable::HFARFWD_ION_ENERGY = 275;
+  Enable::HFARFWD_ION_ENERGY = 100;
 
   // Setting electron beam pipe energy. If you don't know what to set here, leave it at 18
   Enable::HFARBWD_E_ENERGY = 18;
@@ -98,6 +98,7 @@ int Fun4All_G4_EICDetector(
   //
   // Enable::BEAM_COLLISION_SETTING = "eA";
   Enable::BEAM_COLLISION_SETTING = "ep-high-divergence";
+//  Enable::BEAM_COLLISION_SETTING = "ep-high-acceptance";
 
   // Either:
   // read previously generated g4-hits files, in this case it opens a DST and skips
