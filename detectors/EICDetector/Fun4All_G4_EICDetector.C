2afae72fbb270f3f7836b41507a477a253dd1090
billlee77 <wenliang.billlee@gmail.com>
2022-07-07 21:25:15 -0400

    ep 10x100 hi-div IP6

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -85,7 +85,7 @@ int Fun4All_G4_EICDetector(
   Enable::HFARFWD_ION_ENERGY = 100;
 
   // Setting electron beam pipe energy. If you don't know what to set here, leave it at 18
-  Enable::HFARBWD_E_ENERGY = 5;
+  Enable::HFARBWD_E_ENERGY = 10;
 
   // Beam Scattering configuration setting specified by CDR
   //
