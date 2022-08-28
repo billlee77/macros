73f0acecfd7a81ebca1836e1cdd9f2305813f170
billlee77 <wenliang.billlee@gmail.com>
2022-08-28 13:32:53 -0400

    Det1-22.1-5x41GeV-hi_div

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -82,7 +82,7 @@ int Fun4All_G4_EICDetector(
   // Option: 110x18, 110x10, 110x5, 41x5
 
   // Setting proton beam pipe energy. If you don't know what to set here, leave it at 275
-  Enable::HFARFWD_ION_ENERGY = 100;
+  Enable::HFARFWD_ION_ENERGY = 41;
 
   // Setting electron beam pipe energy. If you don't know what to set here, leave it at 18
   Enable::HFARBWD_E_ENERGY = 5;
