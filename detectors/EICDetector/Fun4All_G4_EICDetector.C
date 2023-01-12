86eab59a65f90d7301abffd03413ba3128c37a59
billlee77 <wenliang.billlee@gmail.com>
2023-01-12 03:04:21 -0500

    eHe3 10x150 high divergence

--- detectors/EICDetector/Fun4All_G4_EICDetector.C
+++ detectors/EICDetector/Fun4All_G4_EICDetector.C
@@ -95,6 +95,7 @@ int Fun4All_G4_EICDetector(
   //
   // If you don't know what to put here, set it to ep-high-divergence   
   //
+
   Enable::BEAM_COLLISION_SETTING = "ep-high-divergence";
   //Enable::BEAM_COLLISION_SETTING = "ep-high-acceptance";
   // Enable::BEAM_COLLISION_SETTING = "eA";
